a93db9b9ad0c11d4fd97f8a4237ad49a91836777
beverlyosoria <bevosoria@gmail.com>
2019-08-05 14:20:28 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// state 
+var sum;
+
+// cached elements 
+var sumEl = document.getElementById('sum');
+var inputEl = document.getElementById('input');
+var addEl = document.getElementById('add');
+var subtractEl = document.getElementById('subtract');
+
+// event listeners
+document
+    .getElementById("add")
+    .addEventListener("click", handleAdd);
+
+document
+    .getElementById("subtract")
+    .addEventListener("click", handleSubtract);
+
+//functions
+init();
+
+function init() {
+
+    sum = 0;
+
+    render();
+}
+
+function render() {
+    sumEl.textContent = sum;
+    console.log(Math.sign(sum));
+
+}
+
+
+function handleAdd(event) {
+    var inputValue = document.getElementById('input').value;
+    valueNum = parseInt(inputValue, 10);
+    sum = valueNum + sum;
+
+    render();
+}
+
+function handleSubtract(event) {
+    var inputValue = document.getElementById('input'.value);
+    valueNum = parseInt(inputValue, 10);
+    sum = valueNum - sum;
+    render();
+}
